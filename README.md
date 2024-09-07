df4c379baa5a76c94a0ee0a5f8594a7d4eb885ab
NanGePlus <justicenang@gmail.com>
2024-09-07 15:12:57 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,80 @@
+# 0、项目简介
+实现使用开源的LangFlow框架，零代码实现推荐流量包的智能客服应用，并使用两种方式端到端演示将创建的工作流集成到自己的项目中                                   
+
+
+# 1、基础概念
+## 1.1 LangFlow简介  
+官方介绍:一种用于构建多智能体和RAG应用的可视化框架                                 
+GitHub地址:https://github.com/langflow-ai/langflow                           
+开源协议:MIT协议                      
+
+
+# 2、前期准备工作
+## 2.1 anaconda、pycharm 安装   
+anaconda:提供python虚拟环境，官网下载对应系统版本的安装包安装即可              
+pycharm:提供集成开发环境，官网下载社区版本安装包安装即可            
+可参考如下视频进行安装：              
+https://www.bilibili.com/video/BV1tQWje1ErT/?vd_source=30acb5331e4f5739ebbad50f7cc6b949                     
+
+## 2.2 非GPT大模型(国产大模型)使用方案，OneAPI安装、部署、创建渠道和令牌 
+### （1）OneAPI是什么
+官方介绍：是OpenAI接口的管理、分发系统             
+支持 Azure、Anthropic Claude、Google PaLM 2 & Gemini、智谱 ChatGLM、百度文心一言、讯飞星火认知、阿里通义千问、360 智脑以及腾讯混元             
+### (2)安装、部署、创建渠道和令牌   
+创建渠道：大模型类型(通义千问)、APIKey(通义千问申请的真实有效的APIKey)                 
+创建令牌：创建OneAPI的APIKey，后续代码中直接调用此APIKey                
+### (3)详细介绍可以观看这期视频 
+【GraphRAG+阿里通义千问大模型】构建+检索全流程实操，打造基于知识图谱的本地知识库，本地搜索、全局搜索二合一          
+https://www.bilibili.com/video/BV1yzHxeZEG5/?vd_source=30acb5331e4f5739ebbad50f7cc6b949            
+https://youtu.be/w9CRDbafhPI                 
+
+## 2.3 GPT大模型使用方案            
+可以使用代理的方式，具体代理方案自己选择                          
+可以参考视频《GraphRAG最新版本0.3.0对比实战评测-使用gpt-4o-mini和qwen-plus分别构建近2万字文本知识索引+本地/全局检索对比测试》中推荐的方式：                      
+https://www.bilibili.com/video/BV1zkWse9Enb/?vd_source=30acb5331e4f5739ebbad50f7cc6b949                           
+
+## 2.4 本地大模型使用方案，Ollama     
+Ollama是一个轻量级、跨平台的工具和库，专门为本地大语言模型(LLM)的部署和运行提供支持              
+它旨在简化在本地环境中运行大模型的过程，不需要依赖云服务或外部API，使用户能够更好地掌控和使用大型模型                
+(1)安装Ollama，进入官网https://ollama.com/下载对应系统版本直接安装即可           
+(2)启动Ollama，安装所需要使用的本地模型，执行指令进行安装即可:                
+ollama pull qwen2:latest               
+ollama pull llama3.1:latest              
+本次使用的模型如下:               
+chat模型:qwen2:latest(7b),对应版本有0.5b、1.5b、7b、72b;llama3.1:latest(也就是llama3.1:8b)，对应版本有8b、70b、405b等             
+详细介绍可以观看这期视频【GraphRAG+Ollama】本地开源大模型llama3.1与qwen2构建+检索全流程实操对比评测，打造基于知识图谱的本地知识库，本地搜索、全局搜索二合一：                            
+https://www.bilibili.com/video/BV1mpH9eVES1/?vd_source=30acb5331e4f5739ebbad50f7cc6b949              
+https://youtu.be/thNMan45lWA                            
+
+# 3、项目初始化
+## 3.1 下载源码
+GitHub中下载工程文件到本地，下载地址如下：                
+https://github.com/NanGePlus/LangFlowTest          
+https://gitee.com/NanGePlus/LangFlowTest   
+
+## 3.2 构建项目
+使用pycharm构建一个项目，为项目配置虚拟python环境               
+项目名称：LangFlowTest                 
+
+## 3.3 将相关代码拷贝到项目工程中           
+直接将下载的文件夹中的文件拷贝到新建的项目目录中               
+
+## 3.4 安装项目依赖          
+pip install -r requirements.txt            
+每个软件包后面都指定了本次视频测试中固定的版本号           
+
+# 4、项目测试          
+## 4.1 基础案例:推荐流量包的智能客服测试
+### (1)使用LangFlow搭建推荐流量包的智能客服工作流
+首先，在终端命令行中启动LangFlow服务，执行 langflow run 命令启动                  
+启动成功后，登陆http://127.0.0.1:7860，在服务页面进入store菜单中创建API Key                          
+最后进入My Collectio菜单中新建工程进行工作流编排，测试没问题后，导出工作流json文件                     
+
+### （2）通过调用API接口方式使用创建的工作流
+进入chatTest文件夹下，在使用python fromApiTest.py命令启动脚本前，需根据自己的实际情况调整代码中的如下参数：                        
+**模型相关配置 根据自己的实际情况进行调整:**                            
+
+### （3）通过调用工作流的json文件使用工作流
+进入chatTest文件夹下，在使用python fromJsonTest.py命令启动脚本前，需根据自己的实际情况调整代码中的如下参数：                             
+**模型相关配置 根据自己的实际情况进行调整:**           
+**引入LangFlow的json文件**            
---
+++ chatTest/fromApiTest.py
@@ -0,0 +1,113 @@
+# 使用API接口进行调用
+import requests
+import json
+import logging
+
+# 设置日志模版
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+# 1、模型相关配置
+# # openai模型相关配置 根据自己的实际情况进行调整
+# MODEL_API_BASE = "https://api.wlai.vip/v1"
+# MODEL_CHAT_API_KEY = "sk-EhxvNWXkjzZJADfHA1Ac24Dd0f0b42B2B97f3725D3BcA378"
+# MODEL_CHAT_MODEL = "gpt-4o-mini"
+
+# oneapi相关配置(通义千问为例) 根据自己的实际情况进行调整
+MODEL_API_BASE = "http://139.224.72.218:3000/v1"
+MODEL_CHAT_API_KEY = "sk-DoU00d1PaOMCFrSh68196328E08e443a8886E95761D7F4Bf"
+MODEL_CHAT_MODEL = "qwen-max"
+
+# # 本地大模型配置(Ollama) 根据自己的实际情况进行调整
+# MODEL_API_BASE = "http:localhost:11434/v1"
+# MODEL_CHAT_API_KEY = "ollama"
+# MODEL_CHAT_MODEL = "qwen2"
+
+
+# 2、prompt相关配置  引入外部的文件内容
+sys_file_path = "prompt_template_system.txt"
+user_file_path = "prompt_template_user.txt"
+with open(sys_file_path, 'r', encoding='utf-8') as file:
+    PROMPT_TEMPLATE_TXT_SYS = file.read()
+with open(user_file_path, 'r', encoding='utf-8') as file:
+    PROMPT_TEMPLATE_TXT_USER = file.read()
+
+# 3、API请求相关配置
+url = "http://127.0.0.1:7860/api/v1/run/test"
+headers = {"Content-Type": "application/json"}
+
+
+# 4、构造测试消息体
+input_text = "有没有土豪套餐"
+input_text = "办个200G的套餐"
+# input_text = "有没有流量大的套餐"
+# input_text = "200元以下，流量大的套餐有啥"
+# input_text = "你说那个10G的套餐，叫啥名字"
+# input_text = "你说那个100000000G的套餐，叫啥名字"
+
+data = {
+    "input_value": input_text,
+    "output_type": "chat",
+    "input_type": "chat",
+    "tweaks": {
+        "ChatInput-I1FjN": {
+            "files": "",
+            # "input_value": "",
+            "sender": "User",
+            "sender_name": "User",
+            "session_id": "",
+            "should_store_message": True
+        },
+        "OpenAIModel-0XDe1": {
+            "api_key": MODEL_CHAT_API_KEY,
+            # "input_value": "",
+            "json_mode": False,
+            "max_tokens": None,
+            "model_kwargs": {},
+            "model_name": MODEL_CHAT_MODEL,
+            "openai_api_base": MODEL_API_BASE,
+            "output_schema": {},
+            "seed": 1,
+            "stream": False,
+            "system_message": "",
+            "temperature": 0.2
+        },
+        "ChatOutput-cPe9N": {
+            "data_template": "{text}",
+            # "input_value": "",
+            "sender": "Machine",
+            "sender_name": "AI",
+            "session_id": "",
+            "should_store_message": True
+        },
+        "Prompt-DmUDu": {
+            # "template": "用户问：\n{query}\n",
+            "template": PROMPT_TEMPLATE_TXT_USER,
+            "query": ""
+        },
+        "Prompt-W8qcy": {
+            # "template": "你是一个推荐手机流量套餐的客服代表，你叫NanGe。\n你可以帮助用户选择最合适的流量套餐产品。可以选择的套餐包括：\n经济套餐，月费50元，10G流量；\n畅游套餐，月费180元，100G流量；\n无限套餐，月费300元，1000G流量；\n校园套餐，月费150元，200G流量，仅限在校生。\n\n下面是给你参考的多轮对话的例子：\n例子1:\n客服：有什么可以帮您\n用户：100G套餐有什么\n客服：我们现在有无限套餐，1000G流量，月费300元\n用户：这个套餐是多少钱\n客服：这个套餐是300元\n\n例子2:\n客服：有什么可以帮您\n用户：便宜的套餐有什么\n客服：我们现在有经济套餐，每月50元，10G流量\n用户：100G以上的有什么\n\n例子3:\n客服：有什么可以帮您\n用户：100G以上的套餐有什么\n客服：我们现在有畅游套餐，流量100G，月费180元\n用户：流量最多的呢\n\n例子4:\n客服：有什么可以帮您\n用户：1000元月费的套餐有吗\n客服：没有找到满足1000元价位的产品，是否选择其他套餐\n\n记住：回复时要求你使用口语，亲切一些。不用说“抱歉”。直接给出回答，不用在前面加“NanGe说：”。NO COMMENTS. NO ACKNOWLEDGEMENTS.\n\n"
+            "template":PROMPT_TEMPLATE_TXT_SYS
+        }
+    }
+}
+
+
+# 5、发送post请求进行测试
+response = requests.post(url, headers=headers, data=json.dumps(data))
+# 检查响应状态码
+if response.status_code == 200:
+    try:
+        logger.info(f"输出响应内容是: {response.status_code}\n")
+        logger.info(f"输出响应内容是: {response.json()}\n")
+        # 解析具体回复的内容
+        content = response.json()['outputs'][0]['outputs'][0]['results']['message']['data']['text']
+        logger.info(f"输出响应内容是: {content}\n")
+    except requests.exceptions.JSONDecodeError:
+        # 响应不是JSON格式
+        logger.info("Response content is not valid JSON")
+else:
+    logger.info(f"Request failed with status code {response.status_code}")
+
+
+
